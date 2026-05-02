2b44fdd8682fa777fc12d672f565ba8d2c16e785
Manasa Tolapi <160147011+manasa-tolapi@users.noreply.github.com>
2026-05-02 13:24:54 +0530

    Update main.py - cors fix

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# import health
-from health import server_health_check
 from flask import Flask, jsonify
+from flask_cors import CORS   # ✅ add this
 from blog import get_all_posts, get_post_by_handle
+from health import server_health_check
 
 app = Flask(__name__)
+
+# ✅ enable CORS
+CORS(app)
 
 @app.route('/')
 def home():
@@ -11,24 +14,18 @@ def home():
 
 @app.route('/health')
 def health():
-    report = server_health_check()
-    return report
+    return server_health_check()
 
-
-# ✅ API 1: Get all posts (title, short-desc, image)
 @app.route('/api/posts')
 def posts():
     return jsonify(get_all_posts())
 
-
-# ✅ API 2: Get single post by handle
 @app.route('/api/post/<postHandle>')
 def post_detail(postHandle):
     post = get_post_by_handle(postHandle)
     if post:
         return jsonify(post)
     return jsonify({"error": "Post not found"}), 404
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
